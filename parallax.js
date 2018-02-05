651ef03eca4a64e9a976f6d81e50877801ef2305
Park HyeongJin <jt3205@naver.com>
2018-02-05 23:32:25 +0900

    2018-02-05

--- parallax.js
+++ parallax.js
@@ -2,6 +2,6 @@ $.fn.parallax = function(speed) {
     var parallexEl = $(this);
     $(document).on('scroll', function () {
         var currScrollPos = $(document).scrollTop();
-        parallexEl.css('background-position', '0 ' + -currScrollPos/4 + 'px');
+        parallexEl.css('background-position', '0 ' + -currScrollPos/speed + 'px');
     })
 };
